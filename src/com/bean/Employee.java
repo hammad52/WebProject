dd76fe9c90d2ce658e6ca32feb5e6f6d7fa51d4f
Hammad <hammad52@rediffmail.com>
2018-06-27 01:26:11 +0530

    added Employee class

---
+++ src/com/bean/Employee.java
@@ -0,0 +1,17 @@
+package com.bean;
+
+public class Employee {
+	String name; int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
